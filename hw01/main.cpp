55d1042df6894bed869c1834a85492aef42f3654
duhkneelow <dnilo@my.hpu.edu>
2018-08-29 17:16:10 -1000

    I can't believe I did this

---
+++ hw01/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  hw01 (Basic C++ input / example)
+//
+//  Created by Daniel Nilo on 8/29/18.
+//  Copyright © 2018 Daniel Nilo. All rights reserved.
+//
+
+// system included in iostream
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // code for message
+    std::cout << "Please help me, I'm struggling with C++ because I am inexperienced!\n";
+    // returns message
+    return 0;
+}
