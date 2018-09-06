cbed29b5492fed4effdc3959eba5c012764d1cf9
duhkneelow <dnilo@my.hpu.edu>
2018-09-05 17:10:07 -1000

    CSCI 3731 Assignment #2

---
+++ hw02/main.cc
@@ -0,0 +1,17 @@
+//
+// main.cc
+// hw02 (Declaring functions in header file)
+//
+// Created by Daniel Nilo on 9/4/18.
+// Copyright © 2018 Daniel Nilo. All rights reserved.
+//
+
+#include <stdio.h>
+
+// code to declare sqaured function
+#include "squared.h"
+
+int main(int argc, const char * argv[]) {
+    // code to call squared function
+    printf("The product is %lf\n", squared(3.14));
+}
